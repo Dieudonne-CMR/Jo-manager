9f827924e59f1e95eff7676e4f0db99e6a76a99b
Gilbert <www.ngouogilbert@gmail.com>
2024-10-29 16:52:11 +0000

    travail sur les pages products.php et home.php fin du taf demain

--- template/products.php
+++ template/products.php
@@ -76,187 +76,48 @@
 
 
                 <div class="container-fluid">
-
                     <div class="page-content-wrapper">
-
-                        <div class="row">
-                            <div class="col-xl-3 col-lg-4">
-
-
-
-                                <div class="card">
-
-                                    <div class="card-body">
-
-                                        <div class="border p-3 rounded mt-4">
-
-                                            <h5 class="font-size-16">Recherche</h5>
-
-                                            <div class="search-box me-2 mt-3">
-                                                <div class="position-relative">
-                                                    <input type="text" class="form-control" placeholder="Recherche...">
-                                                    <i class="ti-search search-icon"></i>
-                                                </div>
-                                            </div>
-
-                                        </div>
-
-
-                                        <div class="border p-3 rounded mt-4">
-
-
-                                            <div class="collapse show" id="collapseExample3">
-
-                                            </div>
+                    <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+            
+                                            <h4 class="header-title">Buttons example</h4>
+                                            <p class="card-title-desc">The Buttons extension for DataTables
+                                                provides a common set of options, API methods and styling to display
+                                                buttons on a page that will interact with a DataTable. The core library
+                                                provides the based framework upon which plug-ins can built.
+                                            </p>
+            
+                                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                                <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Position</th>
+                                                    <th>Office</th>
+                                                    <th>Age</th>
+                                                    <th>Start date</th>
+                                                    <th>Salary</th>
+                                                </tr>
+                                                </thead>
+            
+                                                <tbody>
+                                                <tr>
+                                                    <td>Tiger Nixon</td>
+                                                    <td>System Architect</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>61</td>
+                                                    <td>2011/04/25</td>
+                                                    <td>$320,800</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
                                         </div>
                                     </div>
-                                </div>
+                                </div> <!-- end col -->
                             </div>
-
-                            <div class="col-lg-9">
-                                <div class="row" >
-                                    <?php foreach(select_table_where("Produits","Mat_Shop",$mat_shop) as $value):    ?>
-                                        <div class="col-xl-4 col-sm-6">
-                                          <div class="card">
-                                            <div class="card-body">
-                                                <div class="product-img">
-                                                    <div class="product-ribbon  bg-primary">
-                                                        25% Off
-                                                    </div>
-                                                    <img src="<?php echo $image_produit . $value -> Img1 ?>" alt="" class="img-fluid mx-auto d-block">
-                                                </div>
-
-                                                <div class="text-center">
-
-                                                    <a href="#" class="text-dark">
-                                                        <h5 class="font-size-18"><?php echo $value -> Nom_Produit; ?></h5>
-                                                    </a>
-
-                                                    <h4 class="mt-3 mb-0"><?php echo $value -> Prix;   ?><span class="font-size-14 text-muted me-2"><del><?php echo $value -> Prix_Promo;   ?></del></span></h4>
-
-                                                    <div class="mt-3">
-                                                        <ul class="list-inline">
-                                                            <li class="list-inline-item">
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star-half text-warning"></i>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-
-                                                    <div class="product-action mt-2">
-                                                        <div class="d-grid">
-                                                            <a href="javascript:;" class="btn btn-primary"><i class="bx bxs-cart-add"></i>Add to Cart</a>
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    <?php endforeach;    ?>
-                                    <?php /* <div class="col-xl-4 col-sm-6">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div class="product-img">
-                                                    <div class="product-ribbon  bg-primary">
-                                                        25% Off
-                                                    </div>
-
-                                                    <img src="assets/images/product/img-7.png" alt="" class="img-fluid mx-auto d-block">
-                                                </div>
-
-                                                <div class="text-center">
-
-                                                    <a href="#" class="text-dark">
-                                                        <h5 class="font-size-18">Home & Office Chair Green</h5>
-                                                    </a>
-
-                                                    <h4 class="mt-3 mb-0">$200 <span class="font-size-14 text-muted me-2"><del>$240</del></span></h4>
-
-                                                    <div class="mt-3">
-                                                        <ul class="list-inline">
-                                                            <li class="list-inline-item">
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star text-warning"></i>
-                                                                <i class="mdi mdi-star-half text-warning"></i>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-
-                                                    <div class="product-action mt-2">
-                                                        <div class="d-grid">
-                                                            <a href="javascript:;" class="btn btn-primary"><i class="bx bxs-cart-add"></i>Add to Cart</a>
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>*/?> 
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-xl-12">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div class="row">
-                                                    <div class="col-sm-6">
-                                                        <div>
-                                                            <p class="mb-sm-0 mt-2">Page <span class="font-weight-bold">2</span> sur <span class="font-weight-bold">113</span></p>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-6">
-                                                        <div class="float-sm-end">
-                                                            <ul class="pagination pagination-rounded mb-sm-0">
-                                                                <li class="page-item disabled">
-                                                                    <a href="#" class="page-link"><i class="mdi mdi-chevron-left"></i></a>
-                                                                </li>
-                                                                <li class="page-item">
-                                                                    <a href="#" class="page-link">1</a>
-                                                                </li>
-                                                                <li class="page-item active">
-                                                                    <a href="#" class="page-link">2</a>
-                                                                </li>
-                                                                <li class="page-item">
-                                                                    <a href="#" class="page-link">3</a>
-                                                                </li>
-                                                                <li class="page-item">
-                                                                    <a href="#" class="page-link">4</a>
-                                                                </li>
-                                                                <li class="page-item">
-                                                                    <a href="#" class="page-link">5</a>
-                                                                </li>
-                                                                <li class="page-item">
-                                                                    <a href="#" class="page-link"><i class="mdi mdi-chevron-right"></i></a>
-                                                                </li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-
-
-                            </div>
-
-
-                        </div>
                         <!-- end row -->
-
-                    </div>
-
-
                 </div> 
-
                 <!-- container-fluid -->
 
             </div>
@@ -285,6 +146,7 @@
     <script src="assets/libs/metismenu/metisMenu.min.js"></script>
     <script src="assets/libs/simplebar/simplebar.min.js"></script>
     <script src="assets/libs/node-waves/waves.min.js"></script>
+    
 
     <!-- Ion Range Slider-->
     <script src="assets/libs/ion-rangeslider/js/ion.rangeSlider.min.js"></script>
