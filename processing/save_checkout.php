1bb010ef5172441305dd5494653db33c1cb1244b
akilablog <akilablog6@gmail.com>
2024-11-08 17:44:49 +0100

    travail sur modifi-product.php, checkout.php et creation de save_checkout.php

---
+++ processing/save_checkout.php
@@ -0,0 +1,27 @@
+<?php
+    if(!empty($_SESSION['panier'][$mat_shop]));
+        if(isset($_POST['soumettre'])):
+            $recup_panier = $_SESSION['panier'][$mat_shop];     // on recupere les informations du panier envoyees via notre formulaire
+            $nom_client = test_input($_POST['nom_client']);
+            $adresse_client = test_input($_POST['adresse_client']);
+            $tel_client = test_input($_POST['tel_client']);
+            $ville_client = test_input($_POST['ville']);
+
+
+
+        //requete d'insertion des informations de la commande
+
+
+
+
+
+
+
+
+
+
+
+
+    endif;
+endif;
+?>
